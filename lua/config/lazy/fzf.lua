3628377ff2488a932c29be1949dfd41b5604d6ed
FelipeFa6 <felipe.farias.e1@gmail.com>
2024-09-22 19:49:20 -0300

    Readded lsp config

--- lua/config/lazy/fzf.lua
+++ lua/config/lazy/fzf.lua
@@ -1,9 +1,15 @@
 return {
     "ibhagwan/fzf-lua",
     config = function()
-        require("fzf-lua").setup({})
+        require("fzf-lua").setup({ "max-perf" })
+
         vim.keymap.set("n", "<C-p>", require('fzf-lua').files)
         vim.keymap.set("n", "<leader>ps", require('fzf-lua').grep_project)
         vim.keymap.set("n", "<leader>pf", require('fzf-lua').git_files)
+
+        vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>",
+            function() require("fzf-lua").complete_path() end,
+            { silent = true }
+        )
     end
 }
